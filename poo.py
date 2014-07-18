94f14df3beec5e00757980165a58f02b9c96044e
Colten Rouska <colten.rouska@extendhealth.com>
2014-07-18 13:59:53 -0600

    Improper python syntax fix

--- poo.py
+++ poo.py
@@ -14,14 +14,14 @@ io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)
 
 while True:
   (client, address) = s.accept()
-  if io.input(door_pin)
+  if io.input(door_pin):
     print("I not R door")
-    if last_status
+    if last_status:
       client.sendall(False)
       last_status = False
   else:
     print("I am door")
-    if !last_status
+    if !last_status:
       client.sendall(True)
       last_status = True
   time.sleep(0.5)
