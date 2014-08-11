82f7e2d7329f79f894d26346191fb9c377ea3855
Colten Rouska <colten.rouska@extendhealth.com>
2014-08-10 20:16:21 -0600

    Can add to the Queue but cannot remove. Basic structure is in place

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var express = require('express'),
     net = require('net'),
     localHost = '127.0.0.1',
     localport = 8000,
-    client = new net.Socket();
+    client = new net.Socket(),
+    queue = [];
 
 app.use(morgan());
 app.use(express.static(path.join(__dirname, 'html')));
@@ -19,20 +20,28 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-client.connect(localport, localHost);
+// client.connect(localport, localHost);
 
 io.on('connection', function(socket){
-  socket.emit('welcome', {});
-  client.on('data', function(data){
-    var status = data.toString('utf8');
-    var bool = status.toLowerCase() == 'true';
-    socket.emit('status', bool);
-  });
-  //socket.emit('status', false);
+  console.log(socket.client.id)
+  socket.emit('welcome', {id: socket.client.id});
+  // client.on('data', function(data){
+  //   var status = data.toString('utf8');
+  //   var bool = status.toLowerCase() == 'true';
+  //   socket.emit('status', //bool);
+  // });
+  socket.emit('status', false);
   socket.on('disconnect', function(){
-    console.log("User disconnected");
+    console.log(socket.client.id + " User disconnected");
   });
 
+  socket.on('queue-me', function(id){
+    queue.push(id);
+    console.log(id + " has been queued")
+    socket.emit('queued', id);
+  });
+
+  socket.emit("your-turn", {}); // push client id
 });
 http.listen(port, function(){
   console.log("Listening on *:" + port);
--- js/view.js
+++ js/view.js
@@ -1,15 +1,26 @@
 var socket = io(),
-    $status = $('#status');
-  socket.on("welcome", function(msg){
-      
+    $status = $('#status'),
+    theClient = null;
+  socket.on("welcome", function(client){
+    console.log(client);
+    theClient = client;
+    //toastr notification
   });
 
-  socket.on('status', function(open){
+  socket.on('statusUpdate', function(open){
     if ( !open ){
-      $status.addClass('avail');
-      $status.removeClass('not-avail');
-    }else{
       $status.addClass('not-avail');
       $status.removeClass('avail');
+    }else{
+      $status.addClass('avail');
+      $status.removeClass('not-avail');
     }
+  });
+
+  socket.on('queued', function(id){
+    $('#queue-list').append('<li id="'+id+'">'+id+'</li>');
+  });
+
+  $('#queue').click(function(){
+    socket.emit("queue-me", theClient.id);
   });
