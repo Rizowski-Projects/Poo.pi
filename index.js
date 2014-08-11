1b8406a4a00a539f9e243acfc590a39bd0aa892c
Colten Rouska <colten.rouska@extendhealth.com>
2014-08-10 22:43:05 -0600

    Added some styling. Also got Queues to be shared on the view. Problems with sending Queues, as myId is overwritten when new person joins.

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var express = require('express'),
     localHost = '127.0.0.1',
     localport = 8000,
     client = new net.Socket(),
-    queue = [];
+    queue = [],
+    inArray = function(a,b){return!!~a.indexOf(b)};
 
 app.use(morgan());
 app.use(express.static(path.join(__dirname, 'html')));
@@ -23,25 +24,32 @@ app.get('/', function(req, res){
 // client.connect(localport, localHost);
 
 io.on('connection', function(socket){
-  console.log(socket.client.id)
-  socket.emit('welcome', {id: socket.client.id});
-  // client.on('data', function(data){
-  //   var status = data.toString('utf8');
-  //   var bool = status.toLowerCase() == 'true';
-  //   socket.emit('status', //bool);
+  console.log(socket.client.id + " connected")
+  io.emit('welcome', {id: socket.client.id, connected: []});
+  // client.on('data', function(response){
+  //   var status = response.toString('utf8');
+  //   var closed = status.toLowerCase() == 'true';
+  //   socket.emit('statusUpdate', closed);
   // });
-  socket.emit('status', false);
+  socket.emit('statusUpdate', true);
   socket.on('disconnect', function(){
+    //timed removal? Instant removal?
+    io.emit('disconnected', socket.client.id);
     console.log(socket.client.id + " User disconnected");
   });
 
   socket.on('queue-me', function(id){
-    queue.push(id);
-    console.log(id + " has been queued")
-    socket.emit('queued', id);
+    console.log(id);
+    console.log(queue);
+    var result = inArray(queue, id);
+    console.log(result);
+    if(!result){// In coffee convert, item in array check can be used
+      queue.push(id);
+      console.log(id + " has been queued");
+      io.emit('queued', id);
+    }
   });
 
-  socket.emit("your-turn", {}); // push client id
 });
 http.listen(port, function(){
   console.log("Listening on *:" + port);
--- js/view.js
+++ js/view.js
@@ -1,20 +1,29 @@
 var socket = io(),
     $status = $('#status'),
+    $statusMsg = $('#status-msg'),
     theClient = null;
+
   socket.on("welcome", function(client){
     console.log(client);
     theClient = client;
+    $('#online').append("<li id="+client.id+">"+client.id+"</li>");
     //toastr notification
   });
 
   socket.on('statusUpdate', function(open){
     if ( !open ){
+      $statusMsg.html("Closed");
       $status.addClass('not-avail');
       $status.removeClass('avail');
     }else{
+      $statusMsg.html("Open");
       $status.addClass('avail');
       $status.removeClass('not-avail');
     }
+  });
+
+  socket.on('disconnected', function(id){
+    $('#'+id).remove();
   });
 
   socket.on('queued', function(id){
