0af90950ee16141929a7a5da7d25c0fb8f11eb45
Colten Rouska <colten.rouska@extendhealth.com>
2014-08-17 01:49:32 -0600

    Starting to try to figure out sessions

--- index.js
+++ index.js
@@ -6,20 +6,37 @@ var express = require('express'),
     morgan = require('morgan'),
     helpers = require('./js/server/helpers'),
     qManager = require('./js/server/queueManager').import(io),
+    session = require('cookie-session'),
+    route = require('./js/server/routes').import(app),
     port = 8080,
     net = require('net'),
     localHost = '127.0.0.1',
     localport = 8000,
-    client = new net.Socket();
+    client = new net.Socket(),
+    user = {};
 
 app.use(morgan());
+app.use(session({
+    keys: ['meincookie', 'meinOther'],
+    path: '/',
+    cookie: {
+      maxAge: 360000,
+      expires: 360000
+    }
+    // maxAge: 360000,
+}));
+app.get('/', function(req, res, next){
+  console.log(req);
+  user.id = req.sessionID;
+  // res.cookie('wtf', new Date(), {maxAge: 3600000})
+  // console.log(res)
+  // res.sendfile('index.html');
+  next();
+});
 app.use(express.static(path.join(__dirname, 'html')));
 app.use(express.static(path.join(__dirname, 'style')));
 app.use(express.static(path.join(__dirname, 'js')));
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
 
 client.connect(localport, localHost);
 
